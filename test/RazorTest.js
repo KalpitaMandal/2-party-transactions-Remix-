e33bcc815124734dfb999d39c32178bd1d9b712e
KalpitaMandal <49894180+KalpitaMandal@users.noreply.github.com>
2020-06-27 21:39:03 +0530

    Update RazorTest.js

--- test/RazorTest.js
+++ test/RazorTest.js
@@ -2,7 +2,7 @@ const RazorTest = artifacts.require('RazorTest');
 
 contract('RazorTest', () => {
     
-    function sleep(milliseconds) {
+    function sleep(milliseconds) {              //function to sleep so that the interval has expired before testing the MakePayment function
         const date = Date.now();
         let currentDate = null;
         do {
@@ -10,7 +10,7 @@ contract('RazorTest', () => {
         } while (currentDate - date < milliseconds);
     }
 
-    it('Should set the interval values properly', async () => {
+    it('Should set the interval values properly', async () => {       //Checking the CustomPaymentType function
         const razorTest = await RazorTest.deployed();
         const result = await razorTest.CustomPaymentType(1,2);
         var now = new Date();
@@ -20,17 +20,15 @@ contract('RazorTest', () => {
         assert(new_now>now);
     });
 
-    it('Should make a successful payment to the creator', async () => {
+    it('Should make a successful payment to the creator', async () => {         //Checking the MakePayment function
         const razorTest = await RazorTest.deployed();
         let accounts = await web3.eth.getAccounts();
-        await razorTest.CustomPaymentType(6,2);
-        await web3.eth.sendTransaction({from: accounts[0], to: razorTest.address, value: '90000000000000000000'});
+        await razorTest.CustomPaymentType(6,2);                                 //Assigning the interval to be 10 seconds for faster testing of the contract
+        await web3.eth.sendTransaction({from: accounts[0], to: razorTest.address, value: '90000000000000000000'}); 
         let CurrBal = await web3.eth.getBalance('0x92be99E3880aC66D344e143593C17f9f208Db9A1');
         sleep(13000);
         await razorTest.MakePayment('0x92be99E3880aC66D344e143593C17f9f208Db9A1',1);
         const NewBal = await web3.eth.getBalance('0x92be99E3880aC66D344e143593C17f9f208Db9A1');
-        assert(NewBal> CurrBal);
-    });
-
-    
+        assert(NewBal>CurrBal);
+    }); 
 });
