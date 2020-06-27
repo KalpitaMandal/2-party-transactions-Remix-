fb84a97571f54e9b4d43bccc71ed4055bc9dae91
KalpitaMandal <49894180+KalpitaMandal@users.noreply.github.com>
2020-06-27 21:32:55 +0530

    Update RazorTest.js

--- test/RazorTest.js
+++ test/RazorTest.js
@@ -1,6 +1,7 @@
 const RazorTest = artifacts.require('RazorTest');
 
 contract('RazorTest', () => {
+    
     function sleep(milliseconds) {
         const date = Date.now();
         let currentDate = null;
@@ -28,8 +29,6 @@ contract('RazorTest', () => {
         sleep(13000);
         await razorTest.MakePayment('0x92be99E3880aC66D344e143593C17f9f208Db9A1',1);
         const NewBal = await web3.eth.getBalance('0x92be99E3880aC66D344e143593C17f9f208Db9A1');
-        // const addVal = web3.utils.toWei('1','wei');
-        // const Pred = CurrBal + 1;
         assert(NewBal> CurrBal);
     });
 
